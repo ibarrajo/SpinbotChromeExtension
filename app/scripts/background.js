b0d6bdea52c49d05e4e0d03a9941f1c922b7213c
Josue Ibarra <josue@elninja.com>
2014-08-04 17:18:49 -0700

    added api key in options menu

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,19 +1,60 @@
 'use strict';
 
-// chrome.runtime.onInstalled.addListener(function (details) {
-//     console.log('previousVersion', details.previousVersion);
-// });
+chrome.runtime.onInstalled.addListener(function (details) {
+    console.log('previousVersion', details.previousVersion);
+});
 //
-// chrome.tabs.onUpdated.addListener(function (tabId) {
-//     chrome.pageAction.show(tabId);
-// });
+chrome.tabs.onUpdated.addListener(function (tabId) {
+    chrome.pageAction.show(tabId);
+});
 
 // Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+var API_KEY = '00000000000000000000000000000000';
+
+/**
+var options = {
+  spin-cap-words: Boolean,
+  words-to-skip: String,
+  min-percent-change-per-sentence: Int(1-100),
+  action: 'getavailablespins',
+  min-percent-change-per-sentence: Int(1-100),
+  min-percent-change-per-sentence: Int(1-100)
+}
+**/
+function spinbotRequest(options, callback, text) {
+  var xmlhttp;
+  text = text || null;
+  xmlhttp = new XMLHttpRequest();
+
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4) {
+      callback(xmlhttp.responseText);
+    }
+  };
+
+  xmlhttp.open('POST','https://api.spinbot.com', true);
+  xmlhttp.setRequestHeader('x-auth-key', API_KEY);
+
+  for (var action in options) {
+    if (options.hasOwnProperty(action)) {
+      xmlhttp.setRequestHeader('x-' + action, options[action]);
+    }
+  }
+
+  xmlhttp.send(text);
+}
+
+
+function spinBotThis(text) {
+  spinbotRequest({action: 'getavailablespins'}, function(res) {
+
+  });
+}
 
 function copyTextToClipboard(text) {
-  var copyFrom = document.createElement("textarea");
+  var copyFrom = document.createElement('textarea');
   copyFrom.textContent = text;
   var body = document.getElementsByTagName('body')[0];
   body.appendChild(copyFrom);
@@ -34,9 +75,9 @@ chrome.contextMenus.onClicked.addListener(onClickHandler);
 // Set up context menu tree at install time.
 chrome.runtime.onInstalled.addListener(function() {
   chrome.contextMenus.create({
-    "title": "SpinBot This",
-    "contexts": ["selection"],
-    "id": "contextselection"
+    title: 'SpinBot This',
+    contexts: ['selection'],
+    id: 'contextselection'
   });
 
 });
--- app/scripts/options.js
+++ app/scripts/options.js
@@ -5,11 +5,9 @@
 
 // Saves options to chrome.storage
 function save_options() {
-  var color = document.getElementById('color').value;
-  var likesColor = document.getElementById('like').checked;
+  var api_key = document.getElementById('api_key').value;
   chrome.storage.sync.set({
-    favoriteColor: color,
-    likesColor: likesColor
+    api_key: api_key,
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
@@ -23,13 +21,10 @@ function save_options() {
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get({
-    favoriteColor: 'red',
-    likesColor: true
+    api_key: '',
   }, function(items) {
-    document.getElementById('color').value = items.favoriteColor;
-    document.getElementById('like').checked = items.likesColor;
+    document.getElementById('api_key').value = items.api_key;
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
